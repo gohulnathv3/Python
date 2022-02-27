f1f03b92715430e88c84ff031a56ca952e4722b8
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-02-27 23:51:00 +0530

    create guessnumber.py
    This is guess the number program using if, elif, while and random.

---
+++ Python Projects/guessthenumber.py
@@ -0,0 +1,16 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Enter the guess between 1 and {x}: "))
+        if guess < random_number:
+            print("Sry, your guess is low, try with higher values")
+        elif guess > random_number:
+            print("Sry, your guess is high, try with lower values")
+    print("Yay, You guessed the correct number")
+
+
+guess(10)
